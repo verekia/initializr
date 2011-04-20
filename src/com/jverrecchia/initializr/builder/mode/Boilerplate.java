64948ba11c6094a841a0420a8fd77bb6809515e9
Jonathan Verrecchia <verrecchia.j@.paris.sfeir.com>
2011-04-21 01:22:36 +0200

    Simple script, Boilerplate scripts, Boilerplate CSS, Custom CSS modules added

--- src/com/jverrecchia/initializr/builder/mode/Boilerplate.java
+++ src/com/jverrecchia/initializr/builder/mode/Boilerplate.java
@@ -7,12 +7,14 @@ public class Boilerplate extends Mode {
 	public Boilerplate(){
 		this.setName("boilerplate-initializr");
 		this.getFiles().add(new File("index.html", "builder/templates/index.html", true, true));
+		this.getFiles().add(new File("style.css", "builder/templates/style.css", true, true));
 		this.getDefaultModulesNames().add("oldiecond");
 		this.getDefaultModulesNames().add("modernizr");
 		this.getDefaultModulesNames().add("basiccontent");
 		this.getDefaultModulesNames().add("jquery");
 		this.getDefaultModulesNames().add("ddbelatedpng");
 		this.getDefaultModulesNames().add("analytics");
-		this.getDefaultModulesNames().add("scriptjs");
+		this.getDefaultModulesNames().add("boilerplatescripts");
+		this.getDefaultModulesNames().add("boilerplatecss");
 	}
 }
--- src/com/jverrecchia/initializr/builder/mode/Standard.java
+++ src/com/jverrecchia/initializr/builder/mode/Standard.java
@@ -7,7 +7,8 @@ public class Standard extends Mode{
 	public Standard(){
 		this.setName("jverrecchia-initializr");
 		this.getFiles().add(new File("index.html", "builder/templates/index.html", true, true));
-		this.getFiles().add(new File("style.css", "builder/templates/style.css", true, true));
+		this.getFiles().add(new File("css/style.css", "builder/templates/style.css", true, true));
+		this.getFiles().add(new File("script.js", "builder/templates/script.js", true, true));
 		this.getDefaultModulesNames().add("iecond");
 		this.getDefaultModulesNames().add("modernizr");
 	}
