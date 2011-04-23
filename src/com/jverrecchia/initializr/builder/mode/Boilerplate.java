9149071d98e73d7525c0f7d433b1e3285c755651
Jonathan Verrecchia <verrecchia.j@.paris.sfeir.com>
2011-04-23 18:34:03 +0200

    Added handheld module

--- src/com/jverrecchia/initializr/builder/mode/Boilerplate.java
+++ src/com/jverrecchia/initializr/builder/mode/Boilerplate.java
@@ -7,7 +7,7 @@ public class Boilerplate extends Mode {
 	public Boilerplate(){
 		this.setName("boilerplate-initializr");
 		this.getFiles().add(new File("index.html", "builder/templates/index.html", true, true));
-		this.getFiles().add(new File("style.css", "builder/templates/style.css", true, true));
+		this.getFiles().add(new File("css/style.css", "builder/templates/style.css", true, true));
 		this.getDefaultModulesNames().add("oldiecond");
 		this.getDefaultModulesNames().add("modernizr");
 		this.getDefaultModulesNames().add("basiccontent");
