7622f883ac259dc52137a542c1c8e33c52049ed3
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2011-06-26 03:32:12 +0200

    Updated plugins.js

--- war/builder/modules/boilerplatescripts/plugins.js
+++ war/builder/modules/boilerplatescripts/plugins.js
@@ -1,9 +1,11 @@
+
+// usage: log('inside coolFunc', this, arguments);
 window.log = function(){
-  log.history = log.history || [];
+  log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
-  arguments.callee = arguments.callee.caller;  
-  if(this.console) console.log( Array.prototype.slice.call(arguments) );
+  if(this.console) {
+      arguments.callee = arguments.callee.caller;
+      console.log( Array.prototype.slice.call(arguments) );
+  }
 };
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
-
-
