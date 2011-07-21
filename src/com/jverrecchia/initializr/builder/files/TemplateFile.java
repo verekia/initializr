02bd0285bb07a61dfde4602f985c8f7e7ed96942
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2011-07-21 13:25:51 +0200

    Converted all CRLF of builder to LF

--- src/com/jverrecchia/initializr/builder/files/TemplateFile.java
+++ src/com/jverrecchia/initializr/builder/files/TemplateFile.java
@@ -72,7 +72,7 @@ public class TemplateFile {
 				
 				}
 				else{
-					this.setZipContent(this.getZipContent() + str + "\r\n");
+					this.setZipContent(this.getZipContent() + str + "\n");
 				}
 				
 	        }
